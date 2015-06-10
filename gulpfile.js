79f8618f6f3b94cfaa7a3086a29e32d4d1fa9afc
Emily Horsman <me@emilyhorsman.com>
2015-06-10 11:52:40 -0400

    Updated gulpfile to use gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,11 @@
-var gulp         = require('gulp');
-var util         = require('gulp-util');
-var jade         = require('gulp-jade');
-var postcss      = require('gulp-postcss');
-var sourcemaps   = require('gulp-sourcemaps');
-var concat       = require('gulp-concat');
-var entities     = require('gulp-entity-convert');
-var autoprefixer = require('autoprefixer-core');
-var csswring     = require('csswring');
-var nested       = require('postcss-nested');
-var mixins       = require('postcss-mixins');
-var vars         = require('postcss-simple-vars');
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', '*css*', 'autoprefixer-core'],
+    replaceString: /^(gulp|postcss)(-|\.)/
+});
 
 var log = function(err) {
-    util.log(util.colors.red('Error'), err.message);
+    $.util.log(util.colors.red('Error'), err.message);
 };
 
 var paths = {
@@ -36,34 +29,32 @@ var paths = {
 
 var css = function(use_sourcemap, extra) {
     extra = extra || [];
-    var processors = [ autoprefixer(), nested(), mixins(), vars() ].concat(extra);
+    var processors = [ $.autoprefixerCore(), $.nested(), $.mixins(), $.simpleVars() ].concat(extra);
     var g = gulp.src(paths.src.css);
     if (use_sourcemap)
-        return g.pipe(sourcemaps.init())
-                .pipe(concat(paths.concat.css))
-                .pipe(postcss(processors)).on('error', log)
-                .pipe(sourcemaps.write('.'))
+        return g.pipe($.sourcemaps.init())
+                .pipe($.concat(paths.concat.css))
+                .pipe($.postcss(processors)).on('error', log)
+                .pipe($.sourcemaps.write('.'))
                 .pipe(gulp.dest(paths.dest.css));
     else
-        return g.pipe(concat(paths.concat.css))
-                .pipe(postcss(processors)).on('error', log)
+        return g.pipe($.concat(paths.concat.css))
+                .pipe($.postcss(processors)).on('error', log)
                 .pipe(gulp.dest(paths.dest.css));
 };
 
 var resources = function(src, dest) {
-    return gulp.src(src)
-        .pipe(dest)
-        .pipe(gulp.dest(dest));
+    return gulp.src(src).pipe(gulp.dest(dest));
 };
 
 gulp.task('css', function() { css(true); });
-gulp.task('css:production', function() { css(false, [ csswring() ]); });
+gulp.task('css:production', function() { css(false, [ $.csswring() ]); });
 
 gulp.task('markup', function() {
     gulp.src(paths.src.markup)
-        .pipe(jade()).on('error', log)
+        .pipe($.jade()).on('error', log)
         // Convert unicode characters (e.g. not <>, etc) to HTML entities
-        .pipe(entities())
+        .pipe($.entityConvert())
         .pipe(gulp.dest(paths.dest.markup));
 });
 
@@ -76,4 +67,6 @@ gulp.task('watch', function() {
     gulp.watch(paths.src.images, ['images']);
 });
 
+gulp.task('all', ['images', 'icons', 'markup', 'css'])
+gulp.task('all:production', ['images', 'icons', 'markup', 'css:production'])
 gulp.task('default', ['watch']);
